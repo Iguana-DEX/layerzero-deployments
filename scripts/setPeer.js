b10d48db0b94defb9ab3b238e3039170caaeae33
styliann-eth <ns2808@protonmail.com>
2024-02-26 23:23:16 +0000

    Now able to call setPeers()

--- scripts/setPeer.js
+++ scripts/setPeer.js
@@ -1,8 +1,6 @@
 const etherlinkTestnetDeployments = require('../deployments/etherlinkTestnet.json');
 const bscTestnetDeployments = require('../deployments/bscTestnet.json');
 const hre = require('hardhat');
-const ethers = hre.ethers;
-
 
 const endpointIds = {
   sepolia: '40161',
@@ -20,64 +18,55 @@ const endpointIds = {
   optimisticEthereum: '30111',
 };
 
-const lzEndpointAddresses = {
-  sepolia: '0x6edce65403992e310a62460808c4b910d972f10f',
-  bscTestnet: '0x6edce65403992e310a62460808c4b910d972f10f',
-  avalancheFujiTestnet: '0x6edce65403992e310a62460808c4b910d972f10f',
-  polygonMumbai: '0x6edce65403992e310a62460808c4b910d972f10f',
-  arbitrumSepolia: '0x6edce65403992e310a62460808c4b910d972f10f',
-  optimismSepolia: '0x6edce65403992e310a62460808c4b910d972f10f',
-  etherlinkTestnet: '0xec28645346D781674B4272706D8a938dB2BAA2C6',
-  mainnet: '0x1a44076050125825900e736c501f859c50fe728c',
-  bsc: '0x1a44076050125825900e736c501f859c50fe728c',
-  avalanche: '0x1a44076050125825900e736c501f859c50fe728c',
-  polygon: '0x1a44076050125825900e736c501f859c50fe728c',
-  arbitrumOne: '0x1a44076050125825900e736c501f859c50fe728c',
-  optimisticEthereum: '0x1a44076050125825900e736c501f859c50fe728c',
-};
-
 const networkName = hre.network.name;
 const targetNetworkName = process.env.targetNetworkName;
 
 async function main() {
+  const ignTokenDeployments = {
+    etherlinkTestnet: etherlinkTestnetDeployments.IGNToken,
+    bscTestnet: bscTestnetDeployments.IGNToken,
+  };
 
-    const ignTokenDeployments = {
-        etherlinkTestnet: etherlinkTestnetDeployments.IGNToken,
-        bscTestnet: bscTestnetDeployments.IGNToken,
-    }
-  
-    console.log(`Calling setPeer on your ${networkName} OFT for ${targetNetworkName} network.`);
-  
-    const Token = await hre.ethers.getContractFactory('IGNToken');
-    const token = await Token.attach(ignTokenDeployments[networkName]);
-  
-    // const overrides = {
-    //   gasPrice: 100000000000, // Can set this >= to the number read from Ganache window
-    //   gasLimit: 6721975, // Use the same gasLimit as read from Ganache window (or a bit higher if still having issue)
-    // };
+  console.log(
+    `Calling setPeer on your ${networkName} OFT for ${targetNetworkName} network.`
+  );
 
-    console.log(ethers.utils.defaultAbiCoder.encode([ "bytes32" ], ignTokenDeployments[targetNetworkName]));
+  const Token = await hre.ethers.getContractFactory('IGNToken');
+  const token = await Token.attach(ignTokenDeployments[networkName]);
 
-    await token.setPeer(
-        endpointIds[targetNetworkName],
-        ethers.utils.formatBytes32String(ignTokenDeployments[targetNetworkName]),
-        // overrides
+  // const overrides = {
+  //   gasPrice: 100000000000, // Can set this >= to the number read from Ganache window
+  //   gasLimit: 6721975, // Use the same gasLimit as read from Ganache window (or a bit higher if still having issue)
+  // };
+
+  function formatBytes32String(bytes20String) {
+    const bytes32String =
+      '0x' + '0'.repeat(24) + bytes20String.substring(2, bytes20String.length);
+    return bytes32String;
+  }
+
+  await token.setPeer(
+    endpointIds[targetNetworkName],
+    formatBytes32String(ignTokenDeployments[targetNetworkName])
+    // overrides
+  );
+
+  const isPeer = await token.isPeer(
+    endpointIds[targetNetworkName],
+    formatBytes32String(ignTokenDeployments[targetNetworkName])
+  );
+
+  if (isPeer) {
+    console.log(
+      `Your OFT on ${targetNetworkName} has been set as a peer on the ${networkName} OFT.`
     );
-
-    const isPeer = await token.isPeer(
-        endpointIds[targetNetworkName],
-        ethers.utils.formatBytes32String(ignTokenDeployments[targetNetworkName]),
+  } else {
+    console.log(
+      `Failed to set your OFT on ${targetNetworkName} as a peer on the ${networkName} OFT.`
     );
-
-    if (isPeer) {
-        console.log(`Your OFT on ${targetNetworkName} has been set as a peer on the ${networkName} OFT.`);
-    } else {
-        console.log(`Failed to set your OFT on ${targetNetworkName} as a peer on the ${networkName} OFT.`);
-    }
+  }
 }
-  
-  
+
 main()
-    .then(() => console.log('Deployment complete'))
-    .catch((error) => console.error('Error deploying contract:', error));
-  
+  .then(() => console.log('Deployment complete'))
+  .catch((error) => console.error('Error deploying contract:', error));
